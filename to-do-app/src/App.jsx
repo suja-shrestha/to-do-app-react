c8ef4cf96246c9b2b439580ea8341940d235f65e
suja-shrestha <shresthasujal917@gmail.com>
2025-11-09 21:40:10 +0545

    added tailwind

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import './App.css';
 export default function App() {
   const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
